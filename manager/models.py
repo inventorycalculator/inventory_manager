1a65a73b069678bf181471f05c67b4075d446afe
InventoryCalculator <ec2-user@ip-172-31-29-179.ap-northeast-2.compute.internal>
2016-09-30 07:54:04 +0000

    Product, Sale 모델 정의 및 admin에 추가

--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+# User
+from django.contrib.auth.models import User
+
+class Product(models.Model):
+    user = models.ForeignKey(User)
+    productName = models.CharField(max_length = 50)
+    buyCost = models.IntegerField(default = 0)
+    sellCost = models.IntegerField(default = 0)
+    salvageCost = models.IntegerField(default = 0)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.productName
+
+class Sale(models.Model):
+    product = models.ForeignKey(Product)
+    minSale = models.IntegerField(default = 0)
+    probableSale = models.IntegerField(default = 0)
+    maxSale = models.IntegerField(default = 0)
+    date = models.DateTimeField('date created')
+
+    def __str__(self):
+        return self.date
