bdbb35d56e9464a7b7c116931226d243e93a80f3
InventoryCalculator <ec2-user@ip-172-31-29-179.ap-northeast-2.compute.internal>
2016-10-01 02:31:48 +0000

    URL을 manager 폴더로 넣음

--- inventorymanager/urls.py
+++ inventorymanager/urls.py
@@ -14,27 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
-from django.contrib.auth.views import login, logout
-from manager import views
-from manager.forms import LoginForm
 
 urlpatterns = [
+    
+    #home
     url(r'^admin/', admin.site.urls),
 
-    #home
-    url(r'^$', views.home, name='home'),
-
-    #signup/signupcomplete
-    url(r'^signup/$', views.signup, name='signup'),
-    url(r'^signup_complete/$', views.signup_complete, name='signup_complete'),
-
-    #login/logout
-    url(r'^login/$', login, {
-        'authentication_form': LoginForm
-        }, name='login'), 
-    url(r'^logout/$', logout, {
-        'next_page': '/login/', },
-    name='logout'),     
+    #manager
+    url(r'',  include('manager.urls')),
 ]
